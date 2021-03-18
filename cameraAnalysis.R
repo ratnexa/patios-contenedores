67b52a832a239e27498ae7757bb0e44a7a7f6448
Merqueo <samezquita@merqueo.com>
2021-03-18 16:18:13 -0500

    add analysis and plots

--- cameraAnalysis.R
+++ cameraAnalysis.R
@@ -21,6 +21,7 @@ timeData <- get_query({
 	, t.licensePlate
 	, yo.operationType
 	, date_sub(yo.dateTimeStatus4, interval 5 hour) as time #Gets out of the yard
+	, date(date_sub(yo.dateTimeStatus4, interval 5 hour)) as date_trunc
 from yard_operation yo 
 left join truck t on t.id = yo.truckId
 where 
@@ -31,6 +32,7 @@ where
 group by yo.id"
 })
 
+timeData$date_trunc <- as.Date(timeData$date_trunc)
 data <- read.csv('bd_test.csv', sep = ';')
 
 check_plate <- adist(data$plate, timeData$licensePlate)
@@ -40,9 +42,21 @@ rownames(check_plate) <- data$plate
 check_plate <- melt(check_plate, varnames = c('camera', 'reinaldix'))
 check_plate <- check_plate %>% filter(value <= 2)
 
-data <- data %>% left_join(check_plate %>% select(camera, reinaldix), by = c('plate' = 'camera'))
+data <- data %>% left_join(check_plate %>% select(camera, reinaldix), by = c('plate' = 'camera')) %>%
+  mutate(date_trunc = date(register_date))
 
-x <- timeData %>% inner_join(data %>% select(reinaldix, register_date, plate), by = c('licensePlate' = 'reinaldix'))
+x <- timeData %>% inner_join(data %>% select(reinaldix, register_date, plate, date_trunc), by = c('licensePlate' = 'reinaldix',
+                                                                                                  'date_trunc' = 'date_trunc'))
 
 dummy <- x[which(duplicated(x$licensePlate) == FALSE),]
 dummy$diffTime <- difftime(dummy$time, dummy$register_date, units = 'mins')
+
+hist(as.numeric(dummy$diffTime), main = "Diferencia de tiempos con patios",
+     xlab = "Tiempo (minutos)")
+dummy <- dummy %>% filter(abs(diffTime) <= 60)
+
+write.table(dummy, "testCameraTimes.csv", sep = ";", row.names = F)
+
+mean(abs(dummy$diffTime))
+
+boxplot(as.numeric(dummy$diffTime))
